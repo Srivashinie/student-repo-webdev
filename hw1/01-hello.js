f5651954669f8db734e095db37b89426a3751f17
Srivashinie <srivashinie24@gmail.com>
2023-10-08 16:53:12 -0700

    hw1

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has been loaded now.");
+  console.log(
+    "Hello!! I am Srivashinie,a graduate student from India.I have worked on small web application during my undergraduate and have knowldege about basic HTML,CSS and Javascript.I enjoy reading books, watching movies,cooking and trying new activicties. I am exited and happy to be here!"
+  );
 }
